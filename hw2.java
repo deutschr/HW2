6077338e2d4ca1c888e6423929e16076e1d80e9f
RonPC\Ron <rmd97@aol.com>
2016-10-02 22:53:16 -0400

    Working on main and variables for sqrt

---
+++ hw2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public static class hw2
+ {
+  public static void main(String []args){
+      Scanner keyboard = new Scanner(System.in);
+      calcSQRT myObj = new calcSQRT();
+      
+      System.out.print("Please enter the number you want to take the squareroot of: " );
+      double number = keyboard.nextDouble();
+      System.out.print("Please enter how much error will be allowed: ");
+      double error = keyboard.nextDouble();
+      
+      public static double calcSQRT()
+      {
+        double guess;
+        double error;
+        double x;
+        double newGuess = (0.5)*(guess + (x/guess));
+        
+        
+ 
+ }
+}
+
+  
+  
+  
+  
+ 
+
+  
