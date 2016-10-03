2a7cacc6764f35d6b7a1905f417adc4ecfa27a6c
RonPC\Ron <rmd97@aol.com>
2016-10-02 23:10:24 -0400

    added ans print out and created easier to read variables. also made math simpler

--- hw2.java
+++ hw2.java
@@ -1,24 +1,31 @@
 import java.util.Scanner;
 
-public static class hw2
+public class hw2
  {
   public static void main(String []args){
       Scanner keyboard = new Scanner(System.in);
-      calcSQRT myObj = new calcSQRT();
       
       System.out.print("Please enter the number you want to take the squareroot of: " );
       double number = keyboard.nextDouble();
       System.out.print("Please enter how much error will be allowed: ");
       double error = keyboard.nextDouble();
-      
-      public static double calcSQRT()
+      double ans =calcSQRT(number,error,number);
+      System.out.println("Your answer is " + ans);
+  }  
+      private static double calcSQRT(Double guess,Double error,Double number)
       {
-        double guess;
-        double error;
-        double x;
-        double newGuess = (0.5)*(guess + (x/guess));
+        double newGuess = (0.5)*(guess + (number/guess));
         
+        if ((Math.abs(Math.sqrt(number)-newGuess)) <= error)
+        {
+          return newGuess;
+        }
         
+       else
+       {
+         return calcSQRT(newGuess,error,number);
+       }
+       
  
  }
 }
